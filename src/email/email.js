0580b074a3b9d8b0344484afcf3c8be24982ef6d
smitt2767 <smitvekariya2767@gmail.com>
2020-10-16 15:02:22 +0530

    url fixed

--- src/email/email.js
+++ src/email/email.js
@@ -9,7 +9,7 @@ const resetPass = (email, token) => {
         from : 'smitvekariya2767@gmail.com',
         subject : 'Consensus-System-App Paasword Reset',
         html : `<h5>You requested a password reset.</h5>
-                <p>Click this <a href="${process.env.URL}/user/reset_password/${token}">Paasword reset link</a></p>
+                <p>Click this <a href="${process.env.MY_SITE_URL}/user/reset_password/${token}">Paasword reset link</a></p>
         `
     }
     sgMail.send(msg);
